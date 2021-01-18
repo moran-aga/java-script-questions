c31b5772fa6ea92dc17bde68d942ab3f65d5c4ef
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-18 17:32:43 +0200

    question 5
    finished

---
+++ q5.js
@@ -0,0 +1,12 @@
+"use strict"
+
+//5. Write a JavaScript for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen
+
+for (i=0; i<15; i++) {
+    if (i % 2 === 0) {
+        console.log(i + " is even number");
+    }
+    else {
+        console.log(i + " is odd number");
+    }
+}
