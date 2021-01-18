4d4351b94ac93f32a42b6b0a18201d27b5496053
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-18 18:27:06 +0200

    question 10
    finished

---
+++ q10.js
@@ -0,0 +1,8 @@
+"use strict"
+
+//10. Write a JavaScript program to construct the following pattern, using a nested for loop.
+
+
+for (let line = "*"; line.length < 6; line += "*" ) {
+    console.log(line);
+}
