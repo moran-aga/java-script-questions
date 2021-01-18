c62fdc036e1a6c58e7a3999519fd22c101c333d6
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-18 13:38:27 +0200

    question 1
    finished

---
+++ q1.js
@@ -0,0 +1,13 @@
+"use strict"
+//1. Write a JavaScript program that accept two integers and display the larger.
+
+let number1 = Number(prompt("pick a number"));
+let number2 = Number(prompt("pick another number"));
+
+if (number1 > number2) {
+alert(number1);
+}
+else {
+    alert(number2);
+}
+ 
