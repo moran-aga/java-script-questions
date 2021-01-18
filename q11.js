585be869991a841ef185b55f51915ea79e1c9070
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-18 21:22:28 +0200

    question 11
    finished

--- q11.js
+++ q11.js
@@ -2,6 +2,14 @@
 
 //11. Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers
 
-let numberOne = x;
-let numberTwo = y;
-let 
+let numberOne = 15;
+let numberTwo = 5;
+let lower = numberOne < numberTwo ? numberOne : numberTwo;
+let divisor = 1;
+
+    for (let i = 1; i <= lower; i++) {
+        if (numberOne % i === 0 && numberTwo % i === 0) {
+        } divisor = i;
+    } 
+
+    console.log(divisor);
