dfe0d09344b2be2cd02caf8370931eccfd472f3e
moran-aga <77380388+moran-aga@users.noreply.github.com>
2021-01-18 20:36:24 +0200

    question 12
    finished

--- q11.js
+++ q11.js
@@ -2,6 +2,6 @@
 
 //11. Write a JavaScript program to compute the greatest common divisor (GCD) of two positive integers
 
-let numberOne = Number(prompt("first Number"));
-let numberTwo = Number(prompt("Second Number"));
+let numberOne = x;
+let numberTwo = y;
 let 
--- q12.js
+++ q12.js
@@ -2,4 +2,10 @@
 
 //12. Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
 
-let sum = x;
+let sum = 0;
+for (i = 0; i<1000; i++)
+{
+    if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+    }
+}
